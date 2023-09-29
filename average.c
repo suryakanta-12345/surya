29cde24756983c6357402dfe0414abecf5e081a5
suryakanta-12345 <134145638+suryakanta-12345@users.noreply.github.com>
2023-09-29 10:09:40 +0530

    Add files via upload

---
+++ average.c
@@ -0,0 +1,9 @@
+//Average of five number
+#include<stdio.h>
+main()
+{
+	int sub1,sub2,sub3,sub4,sub5;
+	sub1=45,sub2=55,sub3=40,sub4=65,sub5=70;
+	float avg=(sub1+sub2+sub3+sub4+sub5)/5;
+	printf("average of five numbers %f",avg);
+}
