d865e6d9b3cafe8ec03d72c0183179de64e1841e
suryakanta swain <134145638+suryakanta-12345@users.noreply.github.com>
2023-10-05 21:21:20 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,16 @@
+//write a program to find out even or odd number
+#include<stdio.h>
+main()
+{
+	int number;
+	printf("enter the number:");
+	scanf("%d",&number);
+	if(number%2==0)
+	{
+		printf("even number");
+	}
+	else
+	{
+		printf("odd number");
+	}
+}
