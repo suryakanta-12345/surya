63ef93e4608f1cc5ce957cbd7f9dae9e5bdaca15
suryakanta swain <134145638+suryakanta-12345@users.noreply.github.com>
2023-10-08 23:46:53 +0530

    Add files via upload

---
+++ positive.c
@@ -0,0 +1,16 @@
+//write a program to check wheather a number is positive or negative
+#include<stdio.h>
+main()
+{
+	int number;
+	printf("enter the number:");
+	scanf("%d",&number);
+	if(number>0)
+	{
+		printf("positive number");
+	}
+	else
+	{
+		printf("negative number");
+	}
+}
